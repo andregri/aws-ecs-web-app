2a3a79ec607ebefb4819b65cd60ed9225ded5e39
andregri <andrea.grillo96@live.com>
2022-03-29 18:45:32 +0200

    Add POST and GET function to use db

--- backend/main.go
+++ backend/main.go
@@ -15,13 +15,26 @@ const (
 	username = "andrea"
 	password = "password"
 	hostname = "127.0.0.1:3306"
-	dbname   = "ecommerce"
+	dbname   = "phonebook"
 )
+
+var db *sql.DB
 
 func userHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		http.ServeFile(w, r, "form.html")
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		fmt.Fprintf(w, "Get from website! r.PostFrom = %v\n", r.PostForm)
+		name := r.FormValue("name")
+		address, err := getUserAddress(db, name)
+		if err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		fmt.Fprintf(w, "Address = %s\n", address)
 	case "POST":
 		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
 		if err := r.ParseForm(); err != nil {
@@ -31,6 +44,11 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
 		name := r.FormValue("name")
 		address := r.FormValue("address")
+		err := createUser(db, name, address)
+		if err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
 		fmt.Fprintf(w, "Name = %s\n", name)
 		fmt.Fprintf(w, "Address = %s\n", address)
 	default:
@@ -49,7 +67,33 @@ func createDatabase(db *sql.DB) error {
 	return err
 }
 
-func getUserAddress(db *sql.DB, username string) (string, error) {
+func createTable(db *sql.DB) error {
+	query := "CREATE TABLE IF NOT EXISTS user(id int primary key auto_increment, name text, address text)"
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	_, err := db.ExecContext(ctx, query)
+	return err
+}
+
+func createUser(db *sql.DB, name string, address string) error {
+	query := "INSERT INTO user(name, address) VALUES (?, ?)"
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	stmt, err := db.PrepareContext(ctx, query)
+	if err != nil {
+		log.Printf("Error %s when preparing SQL statement", err)
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.ExecContext(ctx, name, address)
+	if err != nil {
+		log.Printf("Error %s when inserting row into products table", err)
+		return err
+	}
+	return nil
+}
+
+func getUserAddress(db *sql.DB, name string) (string, error) {
 	query := `select address from user where name = ?`
 	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelfunc()
@@ -60,7 +104,7 @@ func getUserAddress(db *sql.DB, username string) (string, error) {
 	}
 	defer stmt.Close()
 	var address string
-	row := stmt.QueryRowContext(ctx, username)
+	row := stmt.QueryRowContext(ctx, name)
 	if err := row.Scan(&address); err != nil {
 		return "", err
 	}
@@ -68,7 +112,8 @@ func getUserAddress(db *sql.DB, username string) (string, error) {
 }
 
 func main() {
-	db, err := sql.Open("mysql", dsn(""))
+	var err error
+	db, err = sql.Open("mysql", dsn(""))
 	if err != nil {
 		log.Printf("Error %s when opening DB\n", err)
 		return
@@ -77,6 +122,18 @@ func main() {
 
 	if err := createDatabase(db); err != nil {
 		log.Printf("Error %s when creating DB\n", err)
+		return
+	}
+
+	db, err = sql.Open("mysql", dsn(dbname))
+	if err != nil {
+		log.Printf("Error %s when opening DB\n", err)
+		return
+	}
+	defer db.Close()
+
+	if err := createTable(db); err != nil {
+		log.Printf("Error %s when creating table\n", err)
 		return
 	}
 
